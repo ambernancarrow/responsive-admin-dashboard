3b88ee4b2d369befdf16a2347f47bee321b2ddea
Amber Nancarrow <92060638+ambernancarrow@users.noreply.github.com>
2022-02-23 12:53:42 +1000

    dashboard completion

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# responsive-admin-dashboard
- 
+ # Responsive Dashboard Website
+
+- Responsive Dashboard Website Using HTML CSS & JavaScript
+- Smooth scrolling in each section.
+- Includes a dark and light mode.
+- Compatible with all mobile devices and with a beautiful and pleasant user interface.
+
+![preview img](/preview.png)
---
+++ orders.js
@@ -0,0 +1,62 @@
+const Orders = [
+    {
+        productName: 'Flawless Face Vault',
+        productNumber: '00561',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Euphoric Eyes Collection',
+        productNumber: '06073',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Body Scrub/Lotion Vanilla',
+        productNumber: '09262',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Fresh Dew Skincare Set',
+        productNumber: '00111',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Fresh Touch Foundation 02',
+        productNumber: '00201',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Born This Way Concealer 15',
+        productNumber: '00561',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Runway Ready Mascara Black',
+        productNumber: '00033',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Gradual Tanning Set Fair',
+        productNumber: '09218',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Lippie Stix Red Carpet',
+        productNumber: '00001',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Fresh Touch Foundation 02',
+        productNumber: '00201',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
